c7aaa4303877d7519140c3eaa2fb7b758b185152
Oren Stern <oren.stern2@gmail.com>
2025-07-27 21:22:35 +0300

    removed supabase js

--- src/services/supabase2.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseUrl = 'https://wmehlebxrpnjerybnipq.supabase.co'
-const supabaseKey = 'secretAPIKey'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
-
-
